c7ed8ce16b6d0bf608341e4d102c68b413f89b9f
rishijain2002 <rishijain0652@gmail.com>
2024-09-24 20:46:26 +0530

    modify

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Sidebar from "@/components/Sidebar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,7 +29,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        
+          {/* Sidebar on large screens */}
+          
+
+          {/* Main Content Area */}
+          <div className="flex-grow bg-white">
+            {children}
+          </div>
+        
       </body>
     </html>
   );
